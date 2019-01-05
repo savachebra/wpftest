b06f65624826008de911c2fba82393bd4cf7135e
savachebra <savachebra@gmail.com>
2019-01-06 00:02:17 +0200

    adding stuff

---
+++ WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Factory.StartNew(ChangeCompanyName);
+        }
+
+        private void ChangeCompanyName()
+        {
+            Thread.Sleep(2500);
+            UpdateMessage("Changed");
+        }
+
+        private void UpdateMessage(string changed)
+        {
+            Dispatcher.Invoke(() => txtMessage.Text = changed);
+        }
+    }
+}
